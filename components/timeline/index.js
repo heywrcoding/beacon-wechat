fbad8e6fbf18e79022e2c3f130baa5261ecb9db9
RSzyh <750332908@qq.com>
2019-04-02 18:00:36 +0800

    init timeline component

---
+++ components/timeline/index.js
@@ -0,0 +1,30 @@
+Component({
+  data: {
+    activities: [
+      {
+        id: 0,
+        user: 'RSzyh',
+        operation: '创建了',
+        target: '吃早饭',
+        targetType: '习惯',
+        content: 'fafafafaaaaaaaaaaaaaaaaaasdsadassadadaadeeefuck',
+      },
+      {
+        id: 1,
+        user: 'RSzyh',
+        operation: '完成了',
+        target: '做首页',
+        targetType: '目标',
+        content: 'vzxvzvfafa',
+      },
+      {
+        id: 2,
+        user: 'RSzyh',
+        operation: '创建了',
+        target: '做首页',
+        targetType: '目标',
+        content: 'davokpjoia',
+      },
+    ],
+  },
+});
