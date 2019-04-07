398f61b7ba4f00053fce522615a2e28066475ec2
RSzyh <750332908@qq.com>
2019-04-07 16:10:37 +0800

    remove goAdd because it's unnecessary

--- pages/habit/index/index.js
+++ pages/habit/index/index.js
@@ -1,14 +1,1 @@
-Page({
-  // methods
-  goAdd: function () {
-    if (!getApp().data.hasAuthorize) {
-      wx.switchTab({
-        url: '../../profile/index/index',
-      })
-    } else {
-      wx.navigateTo({
-        url: '../../../components/add/addHabbit'
-      })
-    }
-  },
-});
+Page({});
