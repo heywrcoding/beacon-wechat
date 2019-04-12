dba357e6efb1819e3f50e7b4a9baee2d32160810
RSzyh <750332908@qq.com>
2019-04-12 19:32:53 +0800

    add show and hide items, init create method in creator

--- src/components/creator/index.js
+++ src/components/creator/index.js
@@ -1,1 +1,20 @@
-Component({});
+Component({
+  data: {
+    isShow: false,
+  },
+  methods: {
+    switchDisplay() {
+      this.setData({
+        isShow: !this.data.isShow,
+      });
+    },
+    createHabit() {
+      // TODO: nagivate to create habit page
+      console.log('new habit');
+    },
+    createAim() {
+      // TODO: nagivate to create aim page
+      console.log('new aim');
+    },
+  },
+});
