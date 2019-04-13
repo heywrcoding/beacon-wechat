7d1443ab65468777c2ffc8dfff13aa39dfacc3ba
RSzyh <750332908@qq.com>
2019-04-13 14:13:51 +0800

    lint

--- src/pages/authorize/index.js
+++ src/pages/authorize/index.js
@@ -1,5 +1,5 @@
-//index.js
-//获取应用实例
+// index.js
+// 获取应用实例
 const app = getApp();
 const auth = require('../../../utils/auth.js');
 
@@ -8,52 +8,52 @@ Page({
     motto: 'Welcome to beacon~~~',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
   },
-  onLoad: function () {
+  onLoad() {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
-        hasUserInfo: true
+        hasUserInfo: true,
       });
       auth.login(app.globalData.userInfo).then(() => {
         wx.switchTab({
           url: '/src/pages/home/index',
         });
       });
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+      app.userInfoReadyCallback = (res) => {
         this.setData({
           userInfo: res.userInfo,
-          hasUserInfo: true
+          hasUserInfo: true,
         });
         auth.login(res.userInfo).then(() => {
           wx.switchTab({
             url: '/src/pages/home/index',
           });
         });
-      }
+      };
     } else {
       // 在没有 open-type=getUserInfo 版本的兼容处理
       wx.getUserInfo({
-        success: res => {
+        success: (res) => {
           app.globalData.userInfo = res.userInfo;
           this.setData({
             userInfo: res.userInfo,
-            hasUserInfo: true
+            hasUserInfo: true,
           });
           auth.login(res.userInfo).then(() => {
             wx.switchTab({
               url: '/src/pages/home/index',
             });
           });
-        }
-      })
+        },
+      });
     }
   },
-  getUserInfo: function(e) {
+  getUserInfo(e) {
     console.log(e);
     if (e.detail.userInfo) {
       app.globalData.userInfo = e.detail.userInfo;
@@ -67,5 +67,5 @@ Page({
         });
       });
     }
-  }
-})
+  },
+});
