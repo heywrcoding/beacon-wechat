7816e4aa889f9fc1db19615fcb3b3c8a873467d6
shufan zhang <shufan_zhang@foxmail.com>
2019-04-15 14:12:53 +0800

    ticket#13 activites data pipe from Home page to Timeline component

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,1 +1,45 @@
-Page({});
+const http = require('../../../utils/http.js');
+
+Page({
+  data: {
+    activities: [
+      {
+        id: 0,
+        user: 'RSzyh',
+        operation: '创建了',
+        target: '吃早饭',
+        targetType: '习惯',
+        content: 'fafafafaaaaaaaaaaaaaaaaaasdsadassadadaadeeefuck',
+      },
+      {
+        id: 1,
+        user: 'RSzyh',
+        operation: '完成了',
+        target: '做首页',
+        targetType: '目标',
+        content: 'vzxvzvfafa',
+      },
+      {
+        id: 2,
+        user: 'RSzyh',
+        operation: '创建了',
+        target: '做首页',
+        targetType: '目标',
+        content: 'davokpjoia',
+      },
+    ],
+  },
+
+  onShow() {
+    http.get('activity').then((res) => {
+      console.log(res.data);
+      this.setData({ activities: res.data.activities });
+    });
+
+    // this.timeline = this.selectComponent('#timeline');
+    // console.log(this.timeline);
+    // this.timeline.getActivities(this.data);
+  },
+  onReady() {
+  },
+});
