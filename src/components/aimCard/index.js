817b8276aed7ec149f73b8220feb2388d24331d8
RSzyh <750332908@qq.com>
2019-04-17 16:06:02 +0800

    ticket#19 finish single aim component

--- src/components/aimCard/index.js
+++ src/components/aimCard/index.js
@@ -2,4 +2,26 @@ Component({
   properties: {
     aim: Object,
   },
+  data: {
+    isExpand: false,
+    style: {
+      dropDown: 'drop-down-icon',
+      child: 'child ripple',
+    },
+  },
+  methods: {
+    handleExpand() {
+      const isExpand = !this.data.isExpand;
+      const { style } = this.data;
+      if (isExpand) {
+        style.dropDown = 'drop-down-icon rotate180';
+      } else {
+        style.dropDown = 'drop-down-icon';
+      }
+      this.setData({
+        isExpand,
+        style,
+      });
+    },
+  },
 });
