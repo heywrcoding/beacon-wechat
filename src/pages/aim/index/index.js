9acbe609cb884f66d79d2be575442baeca333d81
RSzyh <750332908@qq.com>
2019-04-17 15:01:01 +0800

    ticket#20 finish pass parameter

--- src/pages/aim/index/index.js
+++ src/pages/aim/index/index.js
@@ -1,1 +1,52 @@
-Page({});
+Page({
+  data: {
+    aim: [
+      {
+        "id": 1,
+        "title": "成为海贼王",
+        "done": false,
+        "open": true,
+        "children": [
+          {
+            "id": 2,
+            "title": "找一艘船",
+            "done": false,
+            "open": true,
+          },
+          {
+            "id": 3,
+            "title": "找一群伙伴",
+            "done": true,
+            "open": true,
+          },
+        ],
+      },
+      {
+        "id": 4,
+        "title": "看blackpink演唱会",
+        "done": false,
+        "open": true,
+        "children": [
+          {
+            "id": 5,
+            "title": "买一台望远镜手机",
+            "done": false,
+            "open": true,
+          },
+          {
+            "id": 6,
+            "title": "买飞机票",
+            "done": false,
+            "open": false,
+          },
+          {
+            "id": 7,
+            "title": "blackpink in your area",
+            "done": false,
+            "open": true,
+          },
+        ],
+      },
+    ],
+  },
+});
