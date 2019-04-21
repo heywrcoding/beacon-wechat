47fb33f45fbc90730ba06de2dc7a4db2587c604c
RSzyh <750332908@qq.com>
2019-04-21 18:02:01 +0800

    ticket#9 finish basic page of friends

--- src/pages/friend/index.js
+++ src/pages/friend/index.js
@@ -1,1 +1,18 @@
-Page({});
+Page({
+  data: {
+    friends: [
+      {
+        uid: 1,
+        nickName: 'RSdot',
+        gender: 1,
+        avatarUrl: 'xxxxx',
+      },
+      {
+        uid: 2,
+        nickName: 'Lagranmoon',
+        gender: 0,
+        avatarUrl: 'xxxxx',
+      },
+    ],
+  },
+});
