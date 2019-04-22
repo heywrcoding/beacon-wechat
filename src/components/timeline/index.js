4b66a7797d314a8cc98a1a01e86f34375cd18d9e
RSzyh <750332908@qq.com>
2019-04-22 17:58:59 +0800

    ticket#4 modified home and timeline style

--- src/components/timeline/index.js
+++ src/components/timeline/index.js
@@ -1,16 +1,5 @@
 Component({
-
   properties: {
     activities: Array,
-  },
-
-  methods: {
-    // onLoad() {
-    //   this.data.activities
-    // }
-    // getActivities(data) {
-    //   console.log(data);
-    //   this.setData({ activities: data.activities });
-    // },
   },
 });
